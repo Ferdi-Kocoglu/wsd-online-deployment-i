d1f851ee7367040656fd6dc812ff5a0e700df04a
Ferdi Kocoglu <ferdi_kocoglu@outlook.com>
2024-09-03 10:09:49 +0300

    last changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello, World");
+  return new Response("Seeking truths beyond meaning of life, you will find 43.");
 };
 
 serve(handleRequest, { port: 7777 });
